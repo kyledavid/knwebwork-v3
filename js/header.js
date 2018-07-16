977bea2d3f5f93302350126c84991318a09d9595
Kyle Steinle <kyledavid022@gmail.com>
2018-07-16 16:52:01 -0700

    refactored nav toggle

--- js/header.js
+++ js/header.js
@@ -4,14 +4,15 @@
         $('.c-main-nav__menu').removeClass('l-main-nav__menu--hidden')
     }
     
-    var showNavMobile = function() {
-        $('.c-main-nav__menu-container').removeClass('l-main-nav__menu-container--hidden')
-        $('.c-main-nav__menu').removeClass('l-main-nav__menu--hidden')
+    var toggleNavMobile = function() {
+        $('.c-main-nav__menu-container').toggleClass('l-main-nav__menu-container--hidden')
+        $('.c-main-nav__menu').toggleClass('l-main-nav__menu--hidden')
+        $('.c-main-nav__menu-toggle').toggleClass('c-main-nav__menu-toggle--clicked')
     }
 
     $(document).ready(function() {
         if(xs.matches) {
-            setTimeout(showNavMobile, 300);
+            $('.c-main-nav__menu-toggle').click(toggleNavMobile)
         } else {
             $(document).mousemove(function() {showNav()})
         }
