bf99d061dad31590ce3aeda3d5f6e26dc25141dc
Kyle Steinle <kyledavid022@gmail.com>
2018-07-18 20:31:41 -0700

    commit

--- archive.php
+++ archive.php
@@ -24,7 +24,7 @@ get_header();
 					while ( have_posts() ) :
 						the_post();
 
-						/*
+						/*zxvcxzcvxczvxzcvxzcvx
 						 * Include the Post-Type-specific template for the content.
 						 * If you want to override this in a child theme, then include a file
 						 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
